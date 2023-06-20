2cbab6e66e2d1f7d25dff17a5f160a0424775c4a
artcabrera <jbcabrera.910@gmail.com>
2023-06-20 21:47:06 +0800

    Fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const config: import('@sveltejs/kit').Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
