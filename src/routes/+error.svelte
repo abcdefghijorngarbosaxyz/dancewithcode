769f8b004c8eee45d676008fafb55c38217e6ec8
artcabrera <jbcabrera.910@gmail.com>
2023-06-21 23:53:45 +0800

    Some Patch on Error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -17,15 +17,19 @@
 
 <h1>{$page.status}—{$page.error?.message}</h1>
 {#if $page.status === 404}
-  Within the confines of an Error 404 page, we encounter fragments of the past—thoughts discarded,
-  designs unfinished, and phrases left unspoken. In this digital realm, as websites grow smaller,
-  they shed layers of the familiar, unveiling new possibilities. But in this minimalistic landscape,
-  we find room for evolution, reinvention, and the allure of what lies beyond the confines of the
-  known. Embrace the mystery, for in the absence of what once was, new possibilities emerge, waiting
-  to be unveiled.
+  <p>
+    Within the confines of an Error 404 page, we encounter fragments of the past—thoughts discarded,
+    designs unfinished, and phrases left unspoken. In this digital realm, as websites grow smaller,
+    they shed layers of the familiar, unveiling new possibilities. But in this minimalistic
+    landscape, we find room for evolution, reinvention, and the allure of what lies beyond the
+    confines of the known. Embrace the mystery, for in the absence of what once was, new
+    possibilities emerge, waiting to be unveiled.
+  </p>
 {:else}
-  In the realm of the unforeseen, an enigmatic error emerges, with its dancing glitches and
-  whispered server secrets, defying explanation. Amidst the uncertainty, embrace the quest for
-  answers, for within the unknown lies the opportunity to uncover hidden truths, forge new
-  understandings, and illuminate uncharted paths.
+  <p>
+    In the realm of the unforeseen, an enigmatic error emerges, with its dancing glitches and
+    whispered server secrets, defying explanation. Amidst the uncertainty, embrace the quest for
+    answers, for within the unknown lies the opportunity to uncover hidden truths, forge new
+    understandings, and illuminate uncharted paths.
+  </p>
 {/if}
