2067a8959bd78b61af5e99b06777cb5a4d0de74b
artcabrera <jbcabrera.910@gmail.com>
2023-06-21 23:30:05 +0800

    Update Home page, again :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import ArrowNe from '../components/icons/ArrowNE.svelte';
 
-  const socials: Array<{ url: string; name: string }> = [
+  type LinkItem = { url: string; name: string; description?: string };
+
+  const socials: Array<LinkItem> = [
     {
       url: 'mailto:hello@jorndoescode.lol',
       name: 'Email'
@@ -17,6 +19,29 @@
     {
       url: 'https://rep.ly/dfntlynotjorn',
       name: 'Rep.ly'
+    }
+  ];
+
+  const projects: Array<LinkItem> = [
+    {
+      url: 'https://dmw-registration.vercel.app',
+      name: 'DMW Registration',
+      description: 'a demo app on Department of Migrant Workers(PH) registration.'
+    },
+    {
+      url: 'https://kazinsbeachresort.com',
+      name: 'Kazins',
+      description: 'a mock website for a resort in Capiz.'
+    },
+    {
+      url: 'https://text-to-asterisks.vercel.app',
+      name: 'Star-inator',
+      description: 'a text masking app, for fun.'
+    },
+    {
+      url: 'https://capsu-clearance.vercel.app',
+      name: 'Clearsys',
+      description: 'a case study app for a state university in Capiz.'
     }
   ];
 </script>
@@ -39,10 +64,12 @@
 
 <h1>Jorn Blaedel Garbosa</h1>
 <p>
-  I am a web developer, focusing in Typescript, holding a steadfast belief in the transformative
-  power of simplicity. Embracing the elegance of minimalism, my website becomes a reflection of my
-  approach to both design and life itself. With every pixel meticulously placed, I passionately
-  strive to create digital experiences that exhale elegance and functionality.
+  I am a web developer, focusing in <a target="_blank" href="https://www.typescriptlang.org/"
+    >Typescript</a
+  >, holding a steadfast belief in the transformative power of simplicity. Embracing the elegance of
+  minimalism, my website becomes a reflection of my approach to both design and life itself. With
+  every pixel meticulously placed, I passionately strive to create digital experiences that exhale
+  elegance and functionality.
 </p>
 
 <hr class="border-dashed" />
@@ -66,7 +93,7 @@
 <p>
   I am fully dedicated to developing and refining my skills as a web developer. Each day is a new
   opportunity for growth, as I actively engage in practice and learning to stay ahead of the curve.
-  So far, I've built some apps, including:
+  These are what I'm building now:
 </p>
 <ul>
   <li>
@@ -75,14 +102,19 @@
       an offline first, free, and open-source desktop music player.
     </p>
   </li>
-  <li>
-    <p>
-      <span class="pr-1"><a target="_blank" href="https://kazinsbeachresort.com">Kazins</a></span>—
-      a mock website for a resort in Capiz.
-    </p>
-  </li>
 </ul>
 <p>
   I also freelance occasionally, it has become my playground, where I apply my expertise and
   collaborate with clients to bring their visions to life.
 </p>
+
+<h2>Projects</h2>
+<ul>
+  {#each projects as project}
+    <li>
+      <p>
+        <span class="pr-1"><a target="_blank" href={project.url}>{project.name}</a></span>— {project.description}
+      </p>
+    </li>
+  {/each}
+</ul>
